04248401bf665de359a624359de881d80aa450cb
mohamediman <imansaabir@gmail.com>
2021-10-25 10:29:45 -0600

    cookie setups with httpOnly set to false

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,7 +30,7 @@ const createAndSendToken = (user, status, req, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
+    httpOnly: false,
     secure: req.secure || req.headers('x-forwarded-proto') === 'https',
   });
 
