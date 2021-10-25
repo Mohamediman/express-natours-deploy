a4c24566c3632d8eb021bd4c9ac69dc04d0fcc4a
mohamediman <imansaabir@gmail.com>
2021-10-25 12:32:26 -0600

    cookie setups w

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,12 +27,13 @@ const createAndSendToken = (user, status, req, res) => {
   //   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
   //   res.cookie('jwt', token, cookieOptions);
 
+  //secure: req.secure || req.headers('x-forwarded-proto') === 'https',
+
   res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: false,
-    secure: req.secure || req.headers('x-forwarded-proto') === 'https',
   });
 
   //=====Remove password from the output
