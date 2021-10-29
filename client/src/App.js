636e6ae7b4cf2b761281f2178a211ff650bd171d
mohamediman <imansaabir@gmail.com>
2021-10-29 11:05:54 -0600

    Memoization and lazyloading

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,64 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useEffect, lazy, Suspense, Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Cookies from 'js-cookie';
-
-import Headers from './components/header/Header';
-import Tours from './components/tours/Tours';
-import TourDetails from './components/tourDetails/TourDetails';
-import Login from './components/auth/Login';
-import Signup from './components/auth/Signup';
-import Account from './components/account/Account';
-import MyBookings from './components/bookings/MyBookings';
-
-import Footer from './components/footer/Footer';
-import Alert from './alerts/Alert';
-import PrivateRoute from './components/routing/PrivateRoute';
-
-import { Provider } from 'react-redux';
-import { getAllTours, loadTour } from './redux/tours/tours.action';
-import { loadUser } from './redux/auth/auth.action';
-// import { loadUser } from './actions/auth';
-import setAuthtoken from './utils/SetAuthToken';
-import store from './redux/Store';
+import { connect } from 'react-redux';
 
 import './App.css';
 
-const App = () => {
+import Footer from './components/footer/Footer';
+import PrivateRoute from './components/routing/PrivateRoute';
+
+import { getAllTours, loadTour } from './redux/tours/tours.action';
+import { loadUser } from './redux/auth/auth.action';
+import Alert from './alerts/Alert';
+import Headers from './components/header/Header';
+import setAuthtoken from './utils/SetAuthToken';
+import ErrorBoundary from './components/error-boundary/error-boundary';
+import Spinner from './components/spinner/spinner';
+
+const Tours = lazy(() => import('./components/tours/Tours'));
+const TourDetails = lazy(() => import('./components/tourDetails/TourDetails'));
+const Login = lazy(() => import('./components/auth/Login'));
+const Signup = lazy(() => import('./components/auth/Signup'));
+const Account = lazy(() => import('./components/account/Account'));
+const MyBookings = lazy(() => import('./components/bookings/MyBookings'));
+
+const App = ({ loadUser, getAllTours, loadTour }) => {
   if (Cookies.get('jwt')) {
     setAuthtoken(Cookies.get('jwt'));
-    store.dispatch(loadUser());
+    loadUser();
   }
 
   useEffect(() => {
-    store.dispatch(getAllTours());
-    store.dispatch(loadTour());
+    getAllTours();
+    loadTour();
 
     // eslint-disable-next-line
   }, []);
 
   return (
-    <Provider store={store}>
-      <Router>
-        <Headers />
-        <Alert />
-        <Switch>
-          <PrivateRoute exact path="/me" component={Account} />
-          <PrivateRoute
-            exact
-            path="/bookings/my-tours"
-            component={MyBookings}
-          />
-          <Route exact path="/" component={Tours} />
-          <Route exact path="/tour/:slug" component={TourDetails} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-        </Switch>
-        <Footer />
-      </Router>
-    </Provider>
+    <Fragment>
+      <Headers />
+      <Alert />
+      <Switch>
+        <ErrorBoundary>
+          <Suspense fallback={Spinner}>
+            <PrivateRoute exact path="/me" component={Account} />
+            <PrivateRoute
+              exact
+              path="/bookings/my-tours"
+              component={MyBookings}
+            />
+            <Route exact path="/" component={Tours} />
+            <Route exact path="/tour/:slug" component={TourDetails} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Suspense>
+        </ErrorBoundary>
+      </Switch>
+      <Footer />
+    </Fragment>
   );
 };
 
-export default App;
+export default connect(null, { loadUser, getAllTours, loadTour })(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,15 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import store from './redux/Store';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
